a9c8038213e5094dbb72ba0e1aecf42ad0de83c4
Mihail Kirilov <bulldozerat@gmail.com>
2019-01-17 10:23:37 +0200

    Get a hero data

--- src/store/heroStore.tsx
+++ src/store/heroStore.tsx
@@ -1,10 +1,14 @@
 import { observable, action } from 'mobx';
 
 export class heroStore {
+  @observable heroData: string[] = [];
+  APIkey: string = "2481591191854227";
+  
   @action fetchSingleCard = async () => {
-    const response = await fetch(`https://superheroapi.com/api.php/2481591191854227/search/spider-man`);
+    const response = await fetch(`https://superheroapi.com/api.php/${this.APIkey}/search/spider-man`);
     const data = await response.json();
 
+    this.heroData = data;
     console.log(data);
     
   }
